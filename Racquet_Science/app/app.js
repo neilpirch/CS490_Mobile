77f83989d07650088f925f062a5b832b6850ec8f
neilpirch <neil.pirch@gmail.com>
2018-12-03 19:26:54 -0600

    Begin Racquet Science

---
+++ Racquet_Science/app/app.js
@@ -0,0 +1,27 @@
+
+var myapp = angular.module('app',[]);
+var url='mongodb://user1:password1@ds119113.mlab.com:119113/cs490';
+myapp.run(function ($http) {
+    // Sends this header with any AJAX request
+    $http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    // Send this header only in post requests. Specifies you are sending a JSON object
+    $http.defaults.headers.post['dataType'] = 'json'
+});
+
+myapp.controller('homeController',function($scope,$http){
+
+    $scope.getData=function(){
+        var req = $http.get('http://127.0.0.1:8081/get');
+        req.success(function(data, status, headers, config) {
+            $scope.matchList = data;
+            console.log(data);
+        });
+        req.error(function(data, status, headers, config) {
+            alert( "failure message: " + JSON.stringify({data: data}));
+        });
+
+    };
+
+});
+
+
---
+++ Racquet_Science/app/mongo.js
@@ -0,0 +1,58 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var bodyParser = require("body-parser");
+var express = require('express');
+var cors = require('cors');
+var app = express();
+
+var url='mongodb://user1:password1@ds119113.mlab.com:19113/cs490';//1.Modify this url with the credentials of your db name and password.
+var ObjectID = require('mongodb').ObjectID;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/create', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        insertDocument(db, req.body, function() {
+            res.write("Successfully inserted");
+            res.end();
+        });
+    });
+});
+
+app.get('/get', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+
+        db.collection('matches').find().toArray(function(err, result){
+            if(err)
+            {
+                res.write("get Failed");
+                res.end();
+            }else
+            {
+
+                res.send(JSON.stringify(result));
+            }
+            console.log("Got All Matches");
+
+        });
+    });
+
+});
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port)
+});
